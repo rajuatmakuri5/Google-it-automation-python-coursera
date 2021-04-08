af96c8e6f01d5c652bc801928ff7313b36757986
rajuatmakuri5 <80907252+rajuatmakuri5@users.noreply.github.com>
2021-04-08 23:55:41 +0800

    Create prime_factors.py

---
+++ Crash Course on Python/prime_factors.py
@@ -0,0 +1,17 @@
+def print_prime_factors(number):
+  # Start with two, which is the first prime
+  factor = 2
+  factor_list = [2]
+  # Keep going until the factor is larger than the number
+  while factor <= number:
+    # Check if factor is a divisor of number
+    if number % factor == 0:
+      # If it is, print it and divide the original number
+      print(factor, end =', ')
+      number = number / factor
+    else:
+      # If it's not, increment the factor by one
+      factor +=1
+  return "Done"
+
+print_prime_factors(100)
